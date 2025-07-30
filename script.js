d3c42977b1c7d017741577891f4bd7b1f5d96fbe
dot-dotdot <175088302+dot-dotdot@users.noreply.github.com>
2025-07-30 16:29:14 +0700

    Add fillGrid function

--- script.js
+++ script.js
@@ -3,3 +3,20 @@
 let sideLength = 16;
 
 const gridContainer = document.querySelector(".grid-container");
+
+fillGrid(sideLength);
+
+function fillGrid(sideLength) {
+    let cellsTotal = sideLength * sideLength
+    for (let i = 1; i <= cellsTotal; i++) {
+        const div = document.createElement("div");
+        div.classList.add("grid-cell");
+        setCellDimensions(div);
+        gridContainer.appendChild(div);
+    }
+}
+
+function setCellDimensions(cell) {
+    cell.style.width = gridContainer.clientWidth / sideLength + "px";
+    cell.style.height = cell.style.width;
+}
