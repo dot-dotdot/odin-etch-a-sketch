518a5ea6aa717b4608f04ccbef20f89b066a51db
dot-dotdot <175088302+dot-dotdot@users.noreply.github.com>
2025-07-31 16:59:36 +0700

    Add drawing functionality

--- script.js
+++ script.js
@@ -11,10 +11,31 @@ function fillGrid(sideLength) {
     for (let i = 1; i <= cellsTotal; i++) {
         const div = document.createElement("div");
         div.classList.add("grid-cell");
+        div.setAttribute("data-color", "");
         div.style.cssText = `width: calc(100% / ${sideLength});
                             height: calc(100% / ${sideLength})`;
         gridContainer.appendChild(div);
     }
+}
+
+gridContainer.addEventListener("mouseover", (event) => {
+    if (event.target.dataset.color !== undefined) {
+        event.target.classList.add("darken");
+        changeOpacity(event.target);
+    }
+});
+
+function changeOpacity(element) {
+    const step = 0.1;
+
+    let currentOpacity = +element.style.opacity;
+    let newOpacity = currentOpacity + step;
+
+    if (newOpacity > 1) {
+        newOpacity = 1;
+    }
+
+    element.style.opacity = newOpacity;
 }
 
 function random(max) {
